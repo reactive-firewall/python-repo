b9568c3d4646c3d3c8599ce4a7d6467224025e15
reactive-firewall <reactive-firewall@users.noreply.github.com>
2018-05-25 22:21:27 -0700

    Changes to be committed: 	modified:   tests/test_usage.py 	- [x] #16 Fixed subtle default value

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -86,9 +86,11 @@ def checkPythonCommand(args=None, stderr=None):
 
 
 @profiling.do_cprofile
-def timePythonCommand(args=[None], stderr=None):
+def timePythonCommand(args=None, stderr=None):
 	"""Function for backend subprocess check_output command
 	with support for coverage and profiling."""
+	if args is None:
+		args = [None]
 	return checkPythonCommand(args, stderr)
 
 

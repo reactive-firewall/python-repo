c2194fe0bc2c889136cfb36b7a7991ad44337e5f
Mr. Walls <reactive-firewall@users.noreply.github.com>
2024-07-10 23:55:09 -0700

    Fix CWE-73 found in PR #37

--- setup.py
+++ setup.py
@@ -28,13 +28,14 @@ except Exception:
 def readFile(filename):
 	"""Helper Function to read files"""
 	theResult = None
-	try:
-		with open(str("""./{}""").format(str(filename))) as f:
-			theResult = f.read()
-	except Exception:
-		theResult = str(
-			"""See https://github.com/reactive-firewall/python-repo/{}"""
-		).format(filename)
+	if filename in ("""README.md""", """LICENSE.md"""):
+		try:
+			with open(str("""./{}""").format(str(filename))) as f:
+				theResult = f.read()
+		except Exception:
+			theResult = str(
+				"""See https://github.com/reactive-firewall/python-repo/{}"""
+			).format(filename)
 	return theResult
 
 

86ef61a02215e8f039283d6f4ec3ac979879a803
Mr. Walls <reactive-firewall@users.noreply.github.com>
2024-09-15 16:24:54 -0700

    [CONFIG] tweaks to sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -27,16 +27,23 @@ needs_sphinx = '4.0'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = [
+				'sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinx.ext.githubpages',
+				'sphinx.ext.autosummary', 'sphinx.ext.doctest', 'sphinx.ext.todo',
+				'sphinx.ext.linkcode', 'sphinx.ext.viewcode'
+			]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = '.md'
+source_suffix = {
+	'.rst': 'restructuredtext',
+	'.md': 'markdown',
+}
 
-# The encoding of source files.
-source_encoding = 'utf-8'
+# The encoding of source files. Official sphinx docs reccomend utf-8-sig.
+source_encoding = 'utf-8-sig'
 
 # The master toctree document.
 master_doc = 'index'
